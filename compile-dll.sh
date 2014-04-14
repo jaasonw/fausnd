bee55fdbf3984338f5be6effbd7396b3f50aa956
Michael Lin <mlin4589@gmail.com>
2014-04-14 00:49:53 -0400

    Updated codeblock file and compile-dll to compile correctly

--- compile-dll.sh
+++ compile-dll.sh
@@ -1,5 +1,5 @@
 set -v
-g++ -O fausnd.cpp -I/c/mingw/sdl/include -L/c/mingw/sdl/lib -ggdb -std=c++11 -Wall -lSDL2main -lSDL2_mixer -lSDL2 -mwindows -lm -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lversion -luuid -s -static-libgcc -static -o fausnd.o
 
-g++ -shared -o fausnd.dll fausnd.o
+g++ -std=c++11 -std=c++0x -Wall -O2 -I/c/mingw/sdl/include -c fausnd.cpp -o fausnd.o
+g++ -shared -Wl,--dll -L/c/mingw/sdl/lib fausnd.o -o fausnd.dll -lSDL2main -lSDL2_mixer -lSDL2 -mwindows -lm -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lversion -luuid -static-libgcc -static
 
